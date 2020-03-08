50f43a7998591e8b0743b37fd23bec4c4ecbdd06
valentine-ezugu <valentineezugu@yahoo.com>
2020-03-08 23:37:14 +0100

    few changes to server.js

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/mygramclient'));
+app.use(express.static(__dirname + '/src'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/mygramclient/index.html'));
+  res.sendFile(path.join(__dirname +'/src'));
 });
-
+console.log(app);
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 4000);
